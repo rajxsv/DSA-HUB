15863a2c421cce4485dbbc22516a928582f602b7
Rajpreet Singh <rajpreetsingh@192.168.1.4>
2024-01-19 02:29:24 +0530

    Added functionalities for signup and login by adding backend api endpoints and  also using jwt token authentication for verifying user, added new modal for user to also have problems.

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,97 +1,26 @@
-import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import "dotenv/config";
+import mongoose, { Schema, mongo } from "mongoose";
 
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      index: true,
-    },
-
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowecase: true,
-      trim: true,
-    },
-
-    fullName: {
-      type: String,
-      required: true,
-      trim: true,
-      index: true,
-    },
-
-    avatar: {
-      type: String, // cloudinary url
-      required: true,
-    },
-
-    coverImage: {
-      type: String, // cloudinary url
-    },
-    
-    watchHistory: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Problem",
-      },
-    ],
-    password: {
-      type: String,
-      required: [true, "Password is required"],
-    },
-    refreshToken: {
-      type: String,
-    },
+const userSchema = new Schema({
+  username: {
+    type: String,
+    require: true,
+    trim : true,
+    lowercase : true
   },
-  {
-    timestamps: true,
-  }
-);
-
-userSchema.pre("save", async function (next) {
-  if (!this.isModified(this.password)) return next();
-
-  this.password = await bcrypt.hash(this.password, 10); // second parameter is salt round.
-  next();
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    min:[8, "Password should be atleast 8 words long"],
+  },
+  problems : [{
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'Problem'
+  }]
 });
-
-userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.password);
-};
-
-userSchema.methods.generateAccessToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-      email: this.email,
-      username: this.username,
-      fullName: this.fullName,
-    },
-    process.env.ACCESS_TOKEN_SECRET,
-    {
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
-    }
-  );
-};
-
-userSchema.methods.generateRefreshToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-    },
-    process.env.REFRESH_TOKEN_SECRET,
-    {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
-    }
-  );
-};
 
 export const User = mongoose.model("User", userSchema);
--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -1,9 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios'
 
 export default function SignUp() {
+
+  const [username,setUsername] = useState()
+  const [email,setEmail] = useState()
+  const [password,setPassword] = useState()
+
+  const[message,setMessage] = useState()
+
+  const navigate = useNavigate()
+
+  const handleSignup = async(e) => {
+    e.preventDefault()
+
+    try {   
+      const response = await axios.post('http://localhost:3000/register',{
+        username,
+        email,
+        password
+      })
+      console.log(response)
+      if(response.status == 200) {
+        setMessage('User created Successfully, Login Now')
+        navigate('/login')
+      }else{
+        setMessage('There was some issue', response.message)
+        navigate('/signup')
+      }
+
+    } catch (error) {
+      console.log(error, 'There was some problem registering user')
+    }
+  }
+
+
   return (
     <div class=" w-full flex flex-col items-center justify-center border-solid p-10 rounded-md border-blue-gray-600">
+      <Link
+        className="font-medium text-black transition-all duration-200 hover:underline"
+        to="/"
+        >
+        Home
+      </Link>
       <h2 class="text-3xl font-bold leading-tight text-black">Sign Up</h2>
       <p class="mt-2 text-base text-gray-600">Already have an account?
       <Link
@@ -13,7 +53,7 @@ export default function SignUp() {
         {"  "}Log In
       </Link>
         </p>
-      <form class="mt-8">
+      <form className="mt-8" onSubmit={handleSignup}>
         <div class="space-y-5">
           <div>
               <label for="email" class="text-base font-medium text-gray-900">
@@ -22,9 +62,11 @@ export default function SignUp() {
               <div class="mt-2">
                 <input
                   class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                  type="email"
-                  placeholder="Email"
-                  id="email"
+                  type="text"
+                  placeholder="username"
+                  id="username"
+                  onChange={(e) => setUsername(e.target.value)}
+                  value={username}
                 />
             </div>
             <div className="mt-4">
@@ -37,6 +79,8 @@ export default function SignUp() {
                   type="email"
                   placeholder="Email"
                   id="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
                 />
               </div>
             </div>
@@ -51,12 +95,14 @@ export default function SignUp() {
                 type="password"
                 placeholder="Password"
                 id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
               />
             </div>
           </div>
           <div className="mt-4">
             <button
-              type="button"
+              type="submit"
               class="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
             >
               Sign Up
@@ -79,6 +125,9 @@ export default function SignUp() {
           </div>
         </div>
       </form>
+      <div>
+        {message}
+      </div>
     </div>
   );
 }
