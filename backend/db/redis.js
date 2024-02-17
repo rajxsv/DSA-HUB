d4e4d6aede7411290c5e54365a0320310075606f
Rajpreet Singh <rajpreetsingh@192.168.1.29>
2024-02-17 11:34:24 +0530

    edited .env and redis.js

--- backend/db/redis.js
+++ backend/db/redis.js
@@ -1,7 +1,5 @@
 import Redis from "ioredis";
 
-const redisClient = new Redis(
-  "rediss://default:49b337785bab488bae54c6dc9029fce6@apn1-driving-hog-33853.upstash.io:33853"
-);
+const redisClient = new Redis(process.env.REDIS_URL);
 
 export default redisClient;
